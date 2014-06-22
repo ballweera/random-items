e21099d3e5fcba1cffb11ed96e1df6e6d73fee7e
Weera Kasetsin <wkasetsin@gmail.com>
2014-06-22 23:51:21 +0700

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Weera Kasetsin on 6/22/14.
+//  Copyright (c) 2014 BigNerdRanch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
